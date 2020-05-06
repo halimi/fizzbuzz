3ca510cf094f86375ccd6fb0b0c089d97ee4c0f6
Imre Halasz <halimi@freemail.hu>
2020-05-06 20:36:28 +0200

    Setup the test environment

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -6,10 +6,14 @@ from fizzbuzz import FizzBuzz
 class FizzBuzzTest(unittest.TestCase):
     """FizzBuzz test class"""
 
+    def setUp(self):
+        """Setup the test environment"""
+        self.fb = FizzBuzz()
+
     def test_fizzbuzz(self):
         """Test the FizzBuzz rules"""
-        self.assertEqual("", FizzBuzz().run(1, 1))
-        self.assertEqual("1", FizzBuzz().run(1, 2))
+        self.assertEqual("", self.fb.run(1, 1))
+        self.assertEqual("1", self.fb.run(1, 2))
 
 
 if __name__ == "__main__":
