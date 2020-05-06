a87b638b667895bb83e1be73e9997b4fa58e85b4
Imre Halasz <halimi@freemail.hu>
2020-05-06 22:40:43 +0200

    It should work on higher numbers

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -19,6 +19,8 @@ class FizzBuzzTest(unittest.TestCase):
         self.assertEqual("1, 2, Fizz, 4, Buzz", self.fb.run(1, 6))
         self.assertEqual("Fizz, 7, 8, Fizz, Buzz", self.fb.run(6, 11))
         self.assertEqual("11, Fizz, 13, 14, FizzBuzz", self.fb.run(11, 16))
+        self.assertEqual("FizzBuzz, 31, 32, Fizz, 34, Buzz",
+                         self.fb.run(30, 36))
 
 
 if __name__ == "__main__":
